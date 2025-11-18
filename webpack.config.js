3022a7733d7c9fce9d903fd8376433fa890bffcb
zhao <mephisto512@aliyun.com>
2025-11-18 23:51:30 +0800

    debug

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -33,6 +34,12 @@ module.exports = {
       template: './src/popup/popup.html',
       filename: 'popup.html',
       chunks: ['popup']
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'manifest.json', to: 'manifest.json' },
+        { from: 'icons', to: 'icons', noErrorOnMissing: true }
+      ]
     })
   ],
   optimization: {
